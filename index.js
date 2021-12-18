47e0a8fd2b4420d55671868dec2897a8d14a268e
licoded <busy.li@foxmail.com>
2021-12-18 23:02:58 +0800

    feat(package.json): 安装一些包

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { MyKoa } from './lib/index.js';
+import { sleep, run } from './lib/utils/index.js';
 
-const app = new MyKoa({});
+const app = new MyKoa({
+  debug: true,
+  launch: {
+    userDataDir: '/Users/lic/dev/products/my-koa-cli/workspace',
+  }
+});
 
 app.use(async ctx => {
+  await run(async (page) => {
+    await page.goto('https://www.baidu.com')
+    await sleep(10000);
+  })
   ctx.body = 'Hello World';
 });
 
 app.listen(3000, () => {
   console.log('server listening on port 3000');
-})
+});
